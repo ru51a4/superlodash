a62aa2a4e01c0e0a01ba88a787f15d7d0d0fe7b3
ru51a4 <ru51a4@mail.ru>
2022-06-05 20:08:32 +0500

    init

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 function deepclone(obj) {
-    let res = {};
-    let createTree = (obj, res) => {
+    let res = (Array.isArray(obj)) ? [] : {};
+    let createTree = (obj, res, prevIsArray) => {
         for (let key in obj) {
             if ((/boolean|number|string|null/).test(typeof obj[key])) {
-                res[key] = obj[key];
+                if (prevIsArray) {
+                    res.push(obj[key])
+                } else {
+                    res[key] = obj[key];
+                }
             } else {
-                res[key] = {};
-                createTree(obj[key], res[key]);
+                res[key] = (Array.isArray(obj[key])) ? [] : {};
+                createTree(obj[key], res[key], Array.isArray(obj[key]));
             }
         }
     }
-    createTree(obj, res);
+    createTree(obj, res, Array.isArray(obj));
     return res;
 }
 
